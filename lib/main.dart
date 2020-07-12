2116ba8f2778b9045d4d88970bfbe0ff4f346094
Hendra <hendra.yani28@gmail.com>
2020-07-12 07:33:38 +0700

    Menambahkan text di body

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("Testing GIT"),),
-        body: Center(),
+        body: Center(child: Text("testing")),
       )
     );
   }
